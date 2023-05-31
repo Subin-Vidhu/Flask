c5e9f4f342937f073e64dda450882f1b2b4b20f6
SUBIN S <85066028+Subin-Vidhu@users.noreply.github.com>
2023-05-31 18:18:26 +0530

    Contacts

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import gspread
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
 gc = gspread.service_account(filename='flask-Udemy.json')
@@ -8,11 +8,17 @@ shDailyUpdates = sh.get_worksheet(0)
 shAutomatedTest = sh.get_worksheet(1)
 #shAutomatedTest.append_row(["31/5/23", "Test","SUBIN","Ongoing"])
 @app.route('/')
-def home():
+def home():    
     return "Hello World"
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index(): 
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        shAutomatedTest.append_row([name, email, message])
+        return "Updated Successfully"
     profile = {
         "about": shAutomatedTest.acell('B1').value,
         "interests": shAutomatedTest.acell('B2').value,
@@ -20,3 +26,7 @@ def index():
         "education": shAutomatedTest.acell('B4').value,
     }
     return render_template('index.html', profile=profile)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
