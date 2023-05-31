69b824028ec392e614447832511c9d1f0d35ae48
SUBIN S <85066028+Subin-Vidhu@users.noreply.github.com>
2023-05-31 16:30:02 +0530

    index page

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello World"
+
+@app.route('/index')
+def index(): 
+    return render_template('index.html')
