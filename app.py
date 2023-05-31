4cdb7a463b9c6cb57b4a8759b365a853951c0048
SUBIN S <85066028+Subin-Vidhu@users.noreply.github.com>
2023-05-31 17:48:25 +0530

    google sheets integration

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+import gspread
 from flask import Flask, render_template
 app = Flask(__name__)
 
+gc = gspread.service_account(filename='flask-Udemy.json')
+sh = gc.open("ML Team Daily Updates")
+shDailyUpdates = sh.get_worksheet(0)
+shAutomatedTest = sh.get_worksheet(1)
+#shAutomatedTest.append_row(["31/5/23", "Test","SUBIN","Ongoing"])
 @app.route('/')
 def home():
     return "Hello World"
 
 @app.route('/index')
 def index(): 
-    return render_template('index.html')
+    profile = {
+        "about": shAutomatedTest.acell('B1').value,
+        "interests": shAutomatedTest.acell('B2').value,
+        "experience": shAutomatedTest.acell('B3').value,
+        "education": shAutomatedTest.acell('B4').value,
+    }
+    return render_template('index.html', profile=profile)
